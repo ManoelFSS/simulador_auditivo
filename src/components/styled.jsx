ba33c0ead2d96a333a5fffbd985d8940d06f381c
Manoel <piloukos45@gmail.com>
2023-06-05 21:21:20 -0300

    teste a

--- src/components/styled.jsx
+++ src/components/styled.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
     display: flex;
+    height: 100vh;
     flex-direction: column;
     max-width: 100%;
     justify-content: center;
